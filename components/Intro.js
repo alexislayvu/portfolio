6feecd9a1698a6a8fc7f8babe02c000bd48d3563
Alexis Lay Vu <alexislayvu@gmail.com>
2024-08-09 20:27:01 -0700

    edit intro

--- components/Intro.js
+++ components/Intro.js
@@ -14,16 +14,10 @@ const Intro = () => {
           hi, <Typewriter words={["i'm alexis."]} cursor />
         </h1>
         <FadeInSection>
-          <h2 className="text-3xl sm:text-4xl text-light-slate">Aspiring Software Developer</h2>
+          <h2 className="text-3xl sm:text-4xl text-light-slate">Aspiring Software Engineer</h2>
           <p className="text-lg md:text-xl text-slate mt-4 max-w-xl mx-auto">
-            I created this portfolio as a final project for a class, and through this experience, I
-            discovered a newfound interest in frontend development. I'm fascinated by its ability to
-            shape digital experiences and create visually appealing websites through code.
-          </p>
-          <p className="text-lg md:text-xl text-slate mt-4 max-w-xl mx-auto">
-            While my current focus is on frontend development, as a new grad, I am open to exploring
-            and contributing to various roles in the tech industry. My journey of learning and
-            growing is ongoing, and I'm excited about the opportunities ahead.
+            I enjoy problem-solving, learning new things, and collaborating with others. I'm eager
+            to contribute to projects in an environment where I can continuously learn and grow.
           </p>
 
           <div className="flex justify-center gap-20 mt-20">

b1eccbe54cc646d44fbc68a2f658fa9fb940b53b
Alexis Lay Vu <alexislayvu@gmail.com>
2025-02-26 19:22:32 -0800

    change project descriptions + reorder projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -7,32 +7,32 @@ const Projects = () => {
     {
       title: 'Object Detector',
       description:
-        'An interactive web application that enables users to detect objects in real-time using webcam feeds or uploaded images. The application features customizable detection settings, allowing users to adjust detection precision according to their needs.',
+        'A React app that detects objects in images and webcam feeds in real-time, with adjustable detection settings for precision.',
       techStack: ['React', 'JavaScript', 'Tensorflow.js', 'COCO-SSD', 'HTML', 'CSS'],
       githubLink: 'https://github.com/alexislayvu/ObjectDetector',
       projectLink: null,
     },
     {
-      title: 'QuizLingo',
-      description:
-        'A language learning app that enhances vocabulary retention through interactive quizzes. The app incorporates relevant images from the Unsplash API to enrich the learning experience and uses the Google ML Kit Translator API to verify user inputs, ensuring accuracy in language assessments.',
-      techStack: ['Kotlin', 'Google ML Kit', 'Unsplash API'],
-      githubLink: 'https://github.com/alexislayvu/quizlingo',
-      projectLink: null,
-    },
-    {
       title: 'TaskPilot',
       description:
-        'A task management application designed with a microservices architecture, utilizing asynchronous task execution for user registration, login, and CRUD operations. The application features secure RESTful API endpoints with JWT authentication, ensuring efficient service interaction and scalability through dedicated databases.',
+        'A Flask-based task management system with asynchronous processing, secure API endpoints, and JWT authentication.',
       techStack: ['Python', 'Flask', 'Celery', 'Redis', 'Nginx', 'MySQL', 'JWT', 'Postman'],
       githubLink: 'https://github.com/alexislayvu/backend-todo-app',
       projectLink: null,
     },
     {
+      title: 'QuizLingo',
+      description:
+        'A Kotlin app that enhances vocabulary learning through interactive quizzes and visual aids for better retention.',
+      techStack: ['Kotlin', 'Google ML Kit Translator API', 'Unsplash API'],
+      githubLink: 'https://github.com/alexislayvu/quizlingo',
+      projectLink: null,
+    },
+    {
       title: 'University Database',
       description:
-        'A university database system that streamlines data management and retrieval using PHP and MySQL. The project features a simple, user-friendly interface designed with HTML and CSS for easy navigation and interaction.',
-      techStack: ['PHP', 'MySQL', 'HTML', 'CSS'],
+        'A web-based university database system with a user-friendly interface for students and faculty, optimized for data organization and retrieval.',
+      techStack: ['HTML', 'CSS', 'MySQL', 'PHP'],
       githubLink: null,
       projectLink: null,
     },
